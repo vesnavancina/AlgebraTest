3d9ab06942821349ae35589a8d3c5b198944bcf5
programeri <programeri@ucione.local>
2017-11-15 18:14:07 +0100

    Init

---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html lang="en">
+<html>
+    <head>
+		<meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        
+		<title>App Name - @yield('title')</title>
+		<link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
+		
+		@stack('styles')
+		
+    
+	</head>
+    <body>
+        <div class="container">
+            @yield('content')
+        </div>
+		@stack('scripts')
+    </body>
+</html>
